8170f990ba2fb271ebb72bf984fe260fb5eadf59
Ignacio Smail <nachitosmail@gmail.com>
2025-06-24 19:55:38 -0300

    update front and about-me

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 document.querySelectorAll('.toggle-info').forEach(button => {
-    button.addEventListener('click', function() {
+    button.addEventListener('click', function () {
         const info = this.nextElementSibling;
         const isExpanded = info.style.display === 'block';
         info.style.display = isExpanded ? 'none' : 'block';
         this.textContent = isExpanded ? '+' : '-';
+    });
+});
+
+// Toggle language
+const langButtons = document.querySelectorAll('.lang-button');
+langButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const lang = button.id.split('-')[1];
+        langButtons.forEach(btn => btn.classList.remove('active'));
+        button.classList.add('active');
+
+        document.querySelectorAll('[data-en]').forEach(el => {
+            el.textContent = el.getAttribute(`data-${lang}`);
+        });
     });
 });
